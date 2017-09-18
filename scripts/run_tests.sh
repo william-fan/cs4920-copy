d742832cf22f61a2930164d1d558a30d90f18bce
Darren Zhu <darrenzhu27@gmail.com>
2017-09-18 21:45:12 +1000

    added tests

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "=== RUNNING TESTS ==="
-echo "Dummy test file. Add tests to a unit test folder and add functionality here for automated running of said tests."
+python3.6 ../status_test.py
 echo "===All tests passed!==="
