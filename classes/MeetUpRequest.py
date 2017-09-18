1c92c9d03b025f23b29dab8205443068f5072cbb
Darren Zhu <darrenzhu27@gmail.com>
2017-09-18 22:22:06 +1000

    Added properties, getters and setters back in, changed all underlying attribute names to be preceded by an _ to avoid infinite recursion.

--- classes/MeetUpRequest.py
+++ classes/MeetUpRequest.py
@@ -1,6 +1,30 @@
 class MeetUpRequest:
 
     def __init__(self, from_id, to_id, status):
-        self.from_id = from_id
-        self.to_id = to_id
-        self.status = status
+        self._from_id = from_id
+        self._to_id = to_id
+        self._status = status
+
+    @property
+    def from_id(self):
+        return self._from_id
+
+    @from_id.setter
+    def from_id(self, from_id):
+        self._from_id = from_id
+
+    @property
+    def to_id(self):
+        return self._to_id
+
+    @to_id.setter
+    def to_id(self, to_id):
+        self._to_id = to_id
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, status):
+        self._status = status
--- classes/UserProfile.py
+++ classes/UserProfile.py
@@ -1,14 +1,107 @@
 class UserProfile:
 
     def __init__(self, user_id, user_type, username, password, first_name, last_name, email, gender, dob, status, imgpath):
-        self.user_id = user_id
-        self.user_type = user_type
-        self.username = username
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.gender = gender
-        self.dob = dob
-        self.status = status
-        self.imgpath = imgpath
+        self._user_id = user_id
+        self._user_type = user_type
+        self._username = username
+        self._password = password
+        self._first__name = first_name
+        self._last_name = last_name
+        self._email = email
+        self._gender = gender
+        self._dob = dob
+        self._status = status
+        self._imgpath = imgpath
+
+    @property
+    def user_id(self):
+        return self._user_id
+
+    @user_id.setter
+    def user_id(self, user_id):
+        self._user_id = user_id
+
+    @property
+    def user_type(self):
+        return self._user_type
+
+    @user_type.setter
+    def user_type(self, user_type):
+        self._user_type = user_type
+
+    @property
+    def username(self):
+        return self._username
+
+    @username.setter
+    def username(self, username):
+        self._username = username
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, password):
+        self._password = password
+
+    @property
+    def first_name(self):
+        return self._first_name
+
+    @first_name.setter
+    def first_name(self, first_name):
+        self._first_name = first_name
+
+    @property
+    def last_name(self):
+        return self._last_name
+
+    @last_name.setter
+    def last_name(self, last_name):
+        self._last_name = last_name
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, email):
+        self._email = email
+
+    @property
+    def gender(self):
+        return self._gender
+
+    @gender.setter
+    def gender(self, gender):
+        self._gender = gender
+
+    @property
+    def dob(self):
+        return self._dob
+
+    @dob.setter
+    def dob(self, dob):
+        self._dob = dob
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, status):
+        self._status = status
+
+    @property
+    def imgpath(self):
+        return self._imgpath
+
+    @imgpath.setter
+    def imgpath(self, imgpath):
+        self._imgpath = imgpath
+
+
+
+
+
