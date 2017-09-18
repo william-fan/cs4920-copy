b5df6f0a376f531cd8e7b6fef29fa044cd93834f
Darren Zhu <darrenzhu27@gmail.com>
2017-09-18 22:08:04 +1000

    Okay test scripts working for real now.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 echo "=== RUNNING TESTS ==="
-python3.6 ../tests/status_test.py
+python3.6 ./scripts/tests/status_test.py
+if [ $? -ne 0 ]; then
+  exit $?
+fi
 echo "===All tests passed!==="
