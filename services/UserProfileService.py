9073d80a375b17ccbf7b3c3b82ddf67710c3dc93
Darren Zhu <darrenzhu27@gmail.com>
2017-10-17 18:01:58 +1100

    Options such as fields to set to private and whether or not to update automatically have been added to the frontend, adding and deleting classes will reevaluate your status on the spot.

--- services/UserProfileService.py
+++ services/UserProfileService.py
@@ -11,6 +11,11 @@ def load_profile(sql_row):
                        sql_row["flags"], sql_row["last_update"])
 
 
+def all_users():
+    sql = "SELECT * FROM user_profile"
+    table = execute_sql(sql)
+    return [load_profile(row) for row in table]
+
 def friends_by_id(user_id):
     sql = "SELECT * FROM user_friend WHERE user_id1="+str(user_id)
     table = execute_sql(sql)
@@ -189,6 +194,7 @@ def delete_todo(id):
 def add_class(user_id, course_name, start_time, day, length, activity):
     sql = "insert into user_class (user_id, course_name, start_time, day, length, activity) values ({}, '{}', {}, {}, {}, '{}')".format(user_id, course_name, start_time, day, length, activity)
     table = execute_sql(sql)
+    update_statuses([find_by_id(user_id)])
 
 
 def search_users(query):
@@ -203,6 +209,7 @@ def search_users(query):
 def delete_class(id):
     sql = "DELETE FROM user_class WHERE id = " + str(id)
     table = execute_sql(sql)
+    update_statuses([find_by_id(id)])
 
 
 def delete_account_sql(id):
