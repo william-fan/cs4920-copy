99379072e9ea954ea90a29ecac09a0b9eda7c7ae
William <z5059967@ad.unsw.edu.au>
2017-10-22 16:25:32 +1100

    Auto change todos

--- static/todo.js
+++ static/todo.js
@@ -1,5 +1,3 @@
-var json = {};
-
 $(document).ready(function(){
     $('#todotable').DataTable({
         "order": [],
@@ -22,23 +20,13 @@ $(document).ready(function(){
 });
 
 $('.change-priority').on('change', function() {
-    $(this).parent().attr('data-order', this.value)
-    if (this.value == 0 || this.value == 1) {
-        $(this).parent().parent().removeClass("table-info table-warning");
-    } else if (this.value == 2) {
-        $(this).parent().parent().removeClass("table-info table-warning").addClass("table-info");
-    } else if (this.value == 3) {
-        $(this).parent().parent().removeClass("table-info table-warning").addClass("table-warning");
-    }
-    $('#todotable').dataTable().api().rows().invalidate('dom').draw();
-    json[this.id] = this.value;
-});
+    var priority = {};
+    priority[this.id] = this.value;
 
-$('#update').click(function() {
     $.ajax({
         type: 'POST',
         contentType: 'application/json',
-        data: JSON.stringify(json),
+        data: JSON.stringify(priority),
         url: '/todo/update',
         dataType: 'json',
         success: function (e) {
@@ -50,8 +38,18 @@ $('#update').click(function() {
             $('#failure').fadeTo(500,1);
         }
     });
-    json = {};
+
+    $(this).parent().attr('data-order', this.value)
+    if (this.value == 0 || this.value == 1) {
+        $(this).parent().parent().removeClass("table-info table-warning");
+    } else if (this.value == 2) {
+        $(this).parent().parent().removeClass("table-info table-warning").addClass("table-info");
+    } else if (this.value == 3) {
+        $(this).parent().parent().removeClass("table-info table-warning").addClass("table-warning");
+    }
+    $('#todotable').dataTable().api().rows().invalidate('dom').draw();
 });
+
 
 $(function(){
     $("[data-hide]").on("click", function(){
@@ -61,12 +59,27 @@ $(function(){
 
 
 $('.remove').on("click", function() {
+    var removejson = {};
+    removejson[this.value] = -1;
     var tableRow = $(this).closest('tr');
     tableRow.find('td').fadeOut('fast',
         function(){
             tableRow.remove();
         }
     );
-    console.log(json);
-    json[this.value] = -1;
+    $.ajax({
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(removejson),
+        url: '/todo/update',
+        dataType: 'json',
+        success: function (e) {
+            $('#failure').fadeOut('fast');
+            $('#success').fadeTo(500,1);
+        },
+        error: function(e) {
+            $('#success').fadeOut('fast');
+            $('#failure').fadeTo(500,1);
+        }
+    });
 });
